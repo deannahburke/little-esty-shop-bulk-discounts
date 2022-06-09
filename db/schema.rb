344fff1399af220ca239d483ab47f7c0bf34e6ad
Deannah Burke <deannah@bierstadtlager.com>
2022-06-09 16:17:18 -0600

    Add bulk discounts table, model and spec

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_04_195915) do
+ActiveRecord::Schema.define(version: 2022_06_09_221356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bulk_discounts", force: :cascade do |t|
+    t.string "name"
+    t.integer "percentage"
+    t.integer "quantity_threshold"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "customers", force: :cascade do |t|
     t.string "first_name"
