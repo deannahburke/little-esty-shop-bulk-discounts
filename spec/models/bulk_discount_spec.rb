21eb4d5408660ed08723d2992178870f637b537e
Deannah Burke <deannah@bierstadtlager.com>
2022-06-09 16:43:24 -0600

    Add merchants foreign key, write validations test

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe BulkDiscount, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it {should validate_presence_of(:name)}
+    it {should validate_presence_of(:percentage)}
+    it {should validate_presence_of(:quantity_threshold)}
+  end
 end
